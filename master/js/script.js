088b238d0da05ceeaa942822b5e1f01d14fd5ec1
Keks <keks@htmlacademy.ru>
2020-10-21 16:56:29 +0300

    :heavy_check_mark: Сборка #master

---
+++ master/js/script.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const sliderItems = document.querySelectorAll('.slider-item');
+const sliderBtns = document.querySelectorAll('.slider-button');
+const sliderBtnsContainer = document.querySelector('.slider-buttons-container');
+
+const featureItems = document.querySelectorAll('.feature-item');
+const featureBtns = document.querySelectorAll('.feature-button');
+const featureBtnsContainer = document.querySelector('.features-buttons-list');
+
+const writeUsModal = document.querySelector('.modal-write-us');
+const writeUsBtn = document.querySelector('.write-us-button');
+
+const mapModal = document.querySelector('.modal-map');
+const mapBtn = document.querySelector('.map-button');
+
+let modalCloseBtn;
+
+const setListenerForSlider = function (items, buttons, container) {
+  container.addEventListener('click', function(evt) {
+    for (let i = 0; i < buttons.length; i++) {
+      if (evt.target === buttons[i]) {
+        evt.preventDefault();
+        for(let j = 0; j < buttons.length; j++) {
+          buttons[j].classList.remove('button-current');
+          items[j].classList.remove('item-current');
+        }
+        buttons[i].classList.add('button-current');
+        items[i].classList.add('item-current');
+      }
+    }
+  });
+};
+
+const setListenerForModal = function (modal, button) {
+  button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.add('modal-current');
+    modalCloseBtn = modal.querySelector('.modal-close');
+    modalCloseBtn.addEventListener('click', function(evt) {
+      evt.preventDefault();
+      modal.classList.remove('modal-current');
+      modal.classList.remove('modal-error');
+    });
+
+    window.addEventListener('keydown', function (evt) {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        modal.classList.remove('modal-current');
+        modal.classList.remove('modal-error');
+      }
+    });
+
+    if (modal.classList.contains('modal-write-us')) {
+      setHandlerForForm(modal);
+    } else {
+      modalCloseBtn.focus();
+    }
+  });
+};
+
+const setHandlerForForm = function (modal) {
+
+  const writeUsForm = modal.querySelector('.write-us-form');
+  const nameInput = modal.querySelector('.write-us-name');
+  const emailInput = modal.querySelector('.write-us-email');
+  const textareaInput = modal.querySelector('.write-us-textarea');
+
+  let isStorageSupport = true;
+  let storageName;
+  let storageEmail;
+
+  try {
+    storageName = localStorage.getItem('name');
+    storageEmail = localStorage.getItem('email');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  if (storageName && storageEmail) {
+    nameInput.value = storageName;
+    emailInput.value = storageEmail;
+    textareaInput.focus();
+  } else {
+    nameInput.focus();
+  }
+
+  writeUsForm.addEventListener('submit', function (evt) {
+    if (!nameInput.value || !emailInput.value || !textareaInput.value) {
+      evt.preventDefault();
+      modal.classList.remove('modal-error');
+      void modal.offsetWidth;
+      modal.classList.add('modal-error');
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem('name', nameInput.value);
+        localStorage.setItem('email', emailInput.value);
+      }
+    }
+  });
+};
+
+setListenerForSlider(sliderItems, sliderBtns, sliderBtnsContainer);
+setListenerForSlider(featureItems, featureBtns, featureBtnsContainer);
+
+setListenerForModal(writeUsModal, writeUsBtn);
+setListenerForModal(mapModal, mapBtn);
